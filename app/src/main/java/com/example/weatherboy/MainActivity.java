d7d34edd5fd37f85a88f02ed8325f6fd6307b7de
baicha <1602134524@qq.com>
2019-12-23 15:41:58 +0800

    weather open

---
+++ app/src/main/java/com/example/weatherboy/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.weatherboy;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import interfaces.heweather.com.interfacesmodule.bean.Code;
+import interfaces.heweather.com.interfacesmodule.bean.Lang;
+import interfaces.heweather.com.interfacesmodule.bean.Unit;
+import interfaces.heweather.com.interfacesmodule.bean.weather.now.Now;
+import interfaces.heweather.com.interfacesmodule.bean.weather.now.NowBase;
+import interfaces.heweather.com.interfacesmodule.view.HeConfig;
+import interfaces.heweather.com.interfacesmodule.view.HeWeather;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG="tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        HeConfig.init("HE1912231319521121","b9cd0713e14843d7b3e871ea138b7130");
+        HeConfig.switchToFreeServerNode();
+        getWeather();
+    }
+
+    public void getWeather(){
+        /**
+         * 实况天气
+         * 实况天气即为当前时间点的天气状况以及温湿风压等气象指数，具体包含的数据：体感温度、
+         * 实测温度、天气状况、风力、风速、风向、相对湿度、大气压强、降水量、能见度等。
+         *
+         * @param context  上下文
+         * @param location 地址详解
+         * @param lang     多语言，默认为简体中文，海外城市默认为英文
+         * @param unit     单位选择，公制（m）或英制（i），默认为公制单位
+         * @param listener 网络访问回调接口
+         */
+
+        HeWeather.getWeatherNow(MainActivity.this, "CN101010100", Lang.CHINESE_SIMPLIFIED , Unit.METRIC , new HeWeather.OnResultWeatherNowBeanListener() {
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "Weather Now onError: ", e);
+            }
+
+            @Override
+            public void onSuccess(Now dataObject) {
+                Log.e(TAG, " Weather Now onSuccess: " + new Gson().toJson(dataObject));
+                //先判断返回的status是否正确，当status正确时获取数据，若status不正确，可查看status对应的Code值找到原因
+                if ( Code.OK.getCode().equalsIgnoreCase(dataObject.getStatus()) ){
+                    //此时返回数据
+                    NowBase now = dataObject.getNow();
+                    Log.e(TAG, "onSuccess: "+now.toString() );
+                } else {
+                    //在此查看返回数据失败的原因
+                    String status = dataObject.getStatus();
+                    Code code = Code.toEnum(status);
+                    Log.e(TAG, "failed code: " + code);
+                }
+            }
+        });
+
+    }
+
+}
